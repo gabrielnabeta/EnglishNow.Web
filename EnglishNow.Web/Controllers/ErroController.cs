11289cff1eaa1bbefc98b8ff8ed456b8d57a2498
gabrielnabeta <gabrielnabeta@gmail.com>
2025-02-24 11:30:47 -0300

    Nova tela de erro

---
+++ EnglishNow.Web/Controllers/ErroController.cs
@@ -0,0 +1,17 @@
+﻿using EnglishNow.Web.Models.Erro;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishNow.Web.Controllers {
+    public class ErroController : Controller {
+        public IActionResult Index() {
+
+            var execptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var model = new ErroViewModel {
+                MensagemErro = execptionHandlerFeature == null ? "Erro inesperado" : execptionHandlerFeature.Error.Message
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ EnglishNow.Web/Models/Erro/ErroViewModel.cs
@@ -0,0 +1,6 @@
+namespace EnglishNow.Web.Models.Erro {
+    class ErroViewModel {
+        public required string MensagemErro { get; set; }
+
+    }
+}
